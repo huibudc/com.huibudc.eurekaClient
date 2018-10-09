875fcc164eea2c427573f4f522446445515a916d
Leon <xiang1990_ok@126.com>
2018-10-09 13:28:50 +0800

    init

---
+++ src/main/java/com/huibudc/eurekaClient/controllers/TestRestful.java
@@ -0,0 +1,23 @@
+package com.huibudc.eurekaClient.controllers;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestRestful {
+    private final Logger logger = Logger.getLogger(getClass());
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String home() {
+        ServiceInstance instance = client.getLocalServiceInstance();
+        logger.info("serviceId" + instance.getServiceId() + "host:post=" + instance.getHost() + ":" + instance.getPort());
+        return "hello spring";
+    }
+}
